c0c3812a1873c8a3241ddad40b6760f064035e1b
Roman Yevchuk <r.yevchuk@gmail.com>
2020-05-28 22:12:11 +0300

    Worked on NewRoom page

--- client/src/pages/new-room.js
+++ client/src/pages/new-room.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
-import {Button, ButtonToolbar, Dropdown} from "react-bootstrap";
+import {Button, ButtonToolbar, Dropdown, FormCheck} from "react-bootstrap";
 import '../styles/new-room.css'
 import {cardSet} from '../const/config.js'
 import Card from "../components/card";
@@ -23,27 +23,44 @@ class NewRoom extends React.Component {
   render() {
     let {isAllCardsActive} = this.state;
 
-
     return (
-      <Container fluid>
+      <Container>
         <h2 className="mt-4 text-xl-center">Create a new room</h2>
         <Row>
-          <Form className="mt-lg-5">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Cards</Form.Label>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1"></Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+          <Form className="mt-lg-1">
+            <Form.Group controlId="formName">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control size="lg" type="text" placeholder="Session name"/>
             </Form.Group>
+
+            <Form.Group controlId="formCheckbox">
+              <Form.Check className="custom-checkbox" type="switch" label="Private session"  />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control size="lg" type="text" placeholder="Session password"/>
+            </Form.Group>
+
+            <h3 className="mt-4 text-xl-center">Cards: </h3>
+
+            {/*<Form.Group controlId="formBasicEmail">*/}
+            {/*  <Form.Label>Cards</Form.Label>*/}
+            {/*  <Dropdown>*/}
+            {/*    <Dropdown.Toggle variant="success" id="dropdown-basic">*/}
+            {/*    </Dropdown.Toggle>*/}
+            {/*    <Dropdown.Menu>*/}
+            {/*      <Dropdown.Item href="#/action-1"></Dropdown.Item>*/}
+            {/*    </Dropdown.Menu>*/}
+            {/*  </Dropdown>*/}
+            {/*</Form.Group>*/}
           </Form>
         </Row>
         <Row>
           <ButtonToolbar>
             <Button
+              variant="outline-dark"
+              size="lg"
               onClick={() => this.onSelectAllCards()}
             >{isAllCardsActive ? "Unselect all" : "Select all"}</Button>
           </ButtonToolbar>
@@ -53,6 +70,11 @@ class NewRoom extends React.Component {
             <Card value={cards} active={isAllCardsActive}/>
           ))}
         </Row>
+        <Row className="mb-2">
+            <Button className="mr-2" variant="outline-dark" size="lg">Create</Button>
+            <Button variant="outline-dark" size="lg">Cancel</Button>
+        </Row>
+
       </Container>
     )
   }
