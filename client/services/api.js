4eb675eb6cbc4893f43b4dda58f090c9a2028a75
Roman Yevchuk <r.yevchuk@gmail.com>
2020-05-30 22:49:08 +0300

    Create api.js and post method

---
+++ client/services/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class Api {
+  constructor() {
+      this.apiUrl = 'http://localhost:8080/api/';
+  }
+
+  post(endpoint, data) {
+    return axios.post(this.getApiEndpoint(endpoint), data)
+      .then((response) => {
+        return { error: null, data: response.data, status: response.status };
+      })
+      .catch((error) => {
+        return { error: error.response };
+      });
+  }
+
+  getApiEndpoint(endpoint) {
+    if (this.apiUrl.endsWith('/') && endpoint.startsWith('/')) {
+      return `${this.apiUrl.slice(0, -1)}${endpoint}`;
+    }
+    if (!this.apiUrl.endsWith('/') && !endpoint.startsWith('/')) {
+      return `${this.apiUrl}/${endpoint}`;
+    }
+    return `${this.apiUrl}${endpoint}`;
+  }
+}
+
+export default new Api();
