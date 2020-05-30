eaae343346ad10906ea750aba2428ba45efef201
Roman Yevchuk <r.yevchuk@gmail.com>
2020-05-30 21:25:43 +0300

    Create back-end skeleton

---
+++ server/src/main/java/ua/com/scrumpoker/ScrumPokerApplication.java
@@ -0,0 +1,13 @@
+package ua.com.scrumpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPokerApplication.class, args);
+	}
+
+}
