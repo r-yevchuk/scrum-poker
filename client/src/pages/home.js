6e1eaa56226f15c03279c1aa11e4f5f512242d24
Roman Yevchuk <r.yevchuk@gmail.com>
2020-08-25 23:03:22 +0300

    Fix fields validation on home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -15,20 +15,18 @@ class Home extends React.Component {
     super(props);
     this.state = {
       errors: {
-        id: '', name: '', connect: ''
+        id: '', name: '', password: '', connect: ''
       },
       user: {
-        id: '',
-        name: '',
-        session: {id: ''}
+        id: '', name: '', password: '', session: {id: ''}
       },
       isPrivateSession: false,
     }
   }
 
   handleChange(event) {
-    const { name, value } = event.target;
-    const { errors } = this.state;
+    const {name, value} = event.target;
+    const {errors} = this.state;
 
     switch (name) {
       case 'id':
@@ -43,10 +41,10 @@ class Home extends React.Component {
         break;
     }
 
+    errors.password = '';
     errors.connect = '';
     this.setState((prevState) => ({
       errors,
-      isPrivateSession: false,
       user: {
         ...prevState.user,
         [name]: value,
@@ -55,18 +53,26 @@ class Home extends React.Component {
   }
 
   onJoinClick() {
-    this.getSession()
+    this.formValidation();
   }
 
-  getSession() {
-    const { user, errors } = this.state;
-
+  formValidation() {
+    const {user, errors} = this.state;
     if (user.id === '') {
       errors.id = 'Id cannot be empty! '
       this.setState({errors});
       return;
     }
 
+    if (user.name === '') {
+      errors.name = 'User name cannot be empty! '
+      this.setState({errors});
+      return;
+    }
+    this.getSession(user, errors);
+  }
+
+  getSession(user, errors) {
     if (user.id !== undefined && user.id > 0 && Number.isInteger(+user.id)) { // '+' convert from string to number
       Api.get('session/' + user.id)
         .then((response) => {
@@ -79,16 +85,38 @@ class Home extends React.Component {
             errors.id = 'Session with such id doesn\'t exist! '
             this.setState({errors});
           } else {
-              user.session.id = user.id;
-              this.createUser(user);
-            if (response.data.password){
-              this.setState({isPrivateSession: true})
-            }
-          }});
+            let password = response.data.password;
+            this.checkPassword(user, password, errors)
+          }
+        });
+    }
+  }
+
+  checkPassword(user, password, errors) {
+    if (this.state.isPrivateSession) {
+      if (user.password.length > 0) {
+        if (user.password === password) {
+          this.createUser(user);
+        } else {
+          errors.password = 'Wrong password! '
+          this.setState({errors});
+        }
+      } else {
+        errors.password = 'This is private session! '
+        this.setState({errors});
+      }
+      return;
+    }
+
+    if (password) {
+      this.setState({isPrivateSession: true})
+    } else {
+      this.createUser(user);
     }
   }
 
   createUser(user) {
+    user.session.id = user.id;
     Api.post('user', user)
       .then((response) => {
         if (response.error) {
@@ -99,60 +127,62 @@ class Home extends React.Component {
   }
 
   render() {
-    const { errors, isPrivateSession } = this.state;
+    const {errors, isPrivateSession} = this.state;
 
     return (
-        <Container>
-          {errors.connect.length > 0 && <Alert className="alert" variant="danger">{errors.connect}</Alert>}
-          <h1 className="text">Scrum poker online</h1>
-          <Row className="p-5">
-            <Form>
-              <Input
-                label="Id"
-                placeholder="Session id"
-                name="id"
-                type="number"
-                isInvalid={errors.id.length > 0}
-                // Prevent enter 'e' symbol
-                onKeyDown={ (evt) => evt.key === 'e' && evt.preventDefault()}
-                onChange={(e) => this.handleChange(e)}
-                errors={errors.id}
-              />
+      <Container>
+        {errors.connect.length > 0 && <Alert className="alert" variant="danger">{errors.connect}</Alert>}
+        <h1 className="text">Scrum poker online</h1>
+        <Row className="p-5">
+          <Form>
+            <Input
+              label="Id"
+              placeholder="Session id"
+              name="id"
+              type="number"
+              isInvalid={errors.id.length > 0}
+              // Prevent enter 'e' symbol
+              onKeyDown={(evt) => evt.key === 'e' && evt.preventDefault()}
+              onChange={(e) => this.handleChange(e)}
+              errors={errors.id}
+            />
 
-              <Input
-                label="Name"
-                placeholder="Please enter your name"
-                name="name"
-                type="text"
-                isInvalid={errors.name.length > 0}
-                onChange={(e) => this.handleChange(e)}
-                errors={errors.name}
-              />
+            <Input
+              label="Name"
+              placeholder="Please enter your name"
+              name="name"
+              type="text"
+              isInvalid={errors.name.length > 0}
+              onChange={(e) => this.handleChange(e)}
+              errors={errors.name}
+            />
 
-              {isPrivateSession &&
-              <Input
-                label="Password"
-                placeholder="Please enter password"
-                name="password"
-                type="text"
-                onChange={(e) => this.handleChange(e)}
-              />}
+            {isPrivateSession &&
+            <Input
+              label="Password"
+              placeholder="Please enter password"
+              name="password"
+              type="text"
+              isInvalid={errors.password.length > 0}
+              onChange={(e) => this.handleChange(e)}
+              errors={errors.password}
+            />}
 
-              <Button
-                className="btn-main mt-2"
-                variant="outline-dark"
-                size="lg"
-                onClick={() => this.onJoinClick()}
-              >Join</Button>
+            <Button
+              className="btn-main mt-2"
+              variant="outline-dark"
+              size="lg"
+              onClick={() => this.onJoinClick()}
+            >Join</Button>
 
-              <p className="separator">or</p>
+            <p className="separator">or</p>
 
-              <Link to="/new-room">
-                <Button className="btn-main" variant="outline-dark" size="lg">Create Room</Button>
-              </Link>
-            </Form>
-          </Row>
-        </Container>
+            <Link to="/new-room">
+              <Button className="btn-main" variant="outline-dark" size="lg">Create Room</Button>
+            </Link>
+          </Form>
+        </Row>
+      </Container>
     )
   }
 }
